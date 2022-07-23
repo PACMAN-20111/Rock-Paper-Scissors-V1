f69765bdb2c880b16b337d3161d8f649a9b682d9
PACMAN-20111 <109835290+PACMAN-20111@users.noreply.github.com>
2022-07-23 09:55:46 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+let Hand = 0
+input.onButtonPressed(Button.A, function () {
+    led.setBrightness(50)
+})
+input.onButtonPressed(Button.AB, function () {
+    led.setBrightness(255)
+})
+input.onButtonPressed(Button.B, function () {
+    led.setBrightness(150)
+})
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
